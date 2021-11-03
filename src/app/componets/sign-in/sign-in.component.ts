20a1ef9808d450a64d0be60a32c3aa25c0413afe
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-04 02:00:11 +0300

    add routing

--- src/app/componets/sign-in/sign-in.component.ts
+++ src/app/componets/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '../user';
+import { User } from 'src/app/class/user';
 import { RouterModule, Routes } from '@angular/router';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
